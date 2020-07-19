8b53f949e70abc723aba4e507b644d5423ea6b92
Kenny W <kennyw.creates@gmail.com>
2020-07-19 17:31:26 -0400

    fizz, buzz, fizzbuzz and bang all running, no errors

--- src/javascript/fizzbuzz.js
+++ src/javascript/fizzbuzz.js
@@ -1,9 +1,9 @@
 for (var i = 1; i < 201; i++) {
     if (i % 3 == 0 & i % 5 == 0) console.log("FizzBuzz");
-    else if (i % 3 == 0) console.log("Fizz");
+    else if (i % 7 == 0) console.log("Fizz");
     else if (i % 5 == 0) console.log("Buzz");
     else if (i % 11 == 0) console.log("Bang");
-    
+    else if (i == 4 ) console.log("Bang, bang bang");
     else console.log(i);
 }
 
